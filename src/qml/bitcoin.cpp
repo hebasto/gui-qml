750efe80241bb918bfce6d54ec64764997869e26
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-08-30 14:44:32 +0300

    qml: Use QmlUtil::GraphicsApi utility function for logging

--- src/qml/bitcoin.cpp
+++ src/qml/bitcoin.cpp
@@ -10,6 +10,7 @@
 #include <node/ui_interface.h>
 #include <noui.h>
 #include <qml/nodemodel.h>
+#include <qml/util.h>
 #include <qt/guiconstants.h>
 #include <qt/guiutil.h>
 #include <qt/initexecutor.h>
@@ -19,9 +20,11 @@
 #include <boost/signals2/connection.hpp>
 #include <memory>
 
+#include <QDebug>
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
+#include <QQuickWindow>
 #include <QStringLiteral>
 #include <QUrl>
 
@@ -145,6 +148,8 @@ int QmlGuiMain(int argc, char* argv[])
     if (engine.rootObjects().isEmpty()) {
         return EXIT_FAILURE;
     }
+
+    qInfo() << "Graphics API in use:" << QmlUtil::GraphicsApi(qobject_cast<QQuickWindow*>(engine.rootObjects().first()));
 
     return qGuiApp->exec();
 }
