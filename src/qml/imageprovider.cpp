5ac8fc7b110b172805620b1ea6dec8d1a4fe04ce
Jarol Rodriguez <jarolrod@tutanota.com>
2022-05-25 01:46:18 -0400

    qml: introduce caret-right icon
    Github-Pull: bitcoin-core/gui-qml#119
    Rebased-From: 303bb4d9a8aa5ad9d1d4bb72c83fc92f44247137

--- src/qml/imageprovider.cpp
+++ src/qml/imageprovider.cpp
@@ -32,6 +32,11 @@ QPixmap ImageProvider::requestPixmap(const QString& id, QSize* size, const QSize
         return QIcon(":/qt/qml/qml/res/icons/caret-left.png").pixmap(requested_size);
     }
 
+    if (id == "caret-right") {
+        *size = requested_size;
+        return QIcon(":/qt/qml/qml/res/icons/caret-right.png").pixmap(requested_size);
+    }
+
     if (id == "info") {
         *size = requested_size;
         return QIcon(":/qt/qml/qml/res/icons/info.png").pixmap(requested_size);
