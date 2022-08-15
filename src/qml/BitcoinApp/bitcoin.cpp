054a47cb583472f6222713a988b8c6216b634073
Jarol Rodriguez <jarolrod@tutanota.com>
2022-08-15 13:10:00 -0400

    qml: out with the stub in with the main
    Does away with the stub file and rename it main.qml
    
    Github-Pull: bitcoin-core/gui-qml#160
    Rebased-From: 447fd0e459fe750f422b03138f24cd4de8a0b61f

--- src/qml/BitcoinApp/bitcoin.cpp
+++ src/qml/BitcoinApp/bitcoin.cpp
@@ -159,7 +159,7 @@ int QmlGuiMain(int argc, char* argv[])
 
     engine.rootContext()->setContextProperty("nodeModel", &node_model);
 
-    engine.load(QUrl(QStringLiteral("qrc:/qt/qml/BitcoinApp/stub.qml")));
+    engine.load(QUrl(QStringLiteral("qrc:/qt/qml/BitcoinApp/main.qml")));
     if (engine.rootObjects().isEmpty()) {
         return EXIT_FAILURE;
     }
