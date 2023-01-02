493a97849c24e1636f821742749899ff0dc446d2
Johnny Carlson <JohnCarlson@pm.me>
2023-01-01 20:49:22 -0500

    qml, test: Build `test_bitcoin-qt`
    TODO: Add to the docs a requirement to install the `qml-module-qttest`
    package in order to run `test_bitcoin-qt`.

---
+++ src/qml/test/onboardingtests.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2023 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_QML_TEST_ONBOARDINGTESTS_H
+#define BITCOIN_QML_TEST_ONBOARDINGTESTS_H
+
+#include <QObject>
+#include <QQmlEngine>
+
+class Setup : public QObject
+{
+    Q_OBJECT
+
+public:
+    Setup();
+    ~Setup() = default;
+
+public Q_SLOTS:
+    void qmlEngineAvailable(QQmlEngine * engine);
+};
+
+#endif // BITCOIN_QML_TEST_ONBOARDINGTESTS_H
